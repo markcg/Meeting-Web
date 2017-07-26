bb0f22f2f1b76f79e3abdbb78a8fcbb448f737d4
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2017-07-26 11:11:39 +0700

    [Add] Field migrations

---
+++ database/migrations/2017_07_26_034755_create_promotion_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePromotionTable extends Migration
+{
+  /**
+  * Run the migrations.
+  *
+  * @return void
+  */
+  public function up()
+  {
+    Schema::create('promotion', function (Blueprint $table) {
+      $table->increments('id');
+      $table->integer('filed_id');
+      $table->string('title');
+      $table->integer('price');
+      $table->text('description');
+      $table->timestamps();
+    });
+  }
+
+  /**
+  * Reverse the migrations.
+  *
+  * @return void
+  */
+  public function down()
+  {
+    Schema::drop('promotion');
+  }
+}
